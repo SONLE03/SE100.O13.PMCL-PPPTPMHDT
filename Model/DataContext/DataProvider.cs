e3017942832485d3efb1a141af829c1b3c44bf2f
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2023-10-16 20:54:58 +0700

    Initial project and config database

---
+++ Model/DataContext/DataProvider.cs
@@ -0,0 +1,32 @@
+﻿using SE100.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE100.Model.DataContext
+{
+    public class DataProvider
+    {
+        private static QLCFEntities instance;
+
+        public static QLCFEntities Instance
+        {
+            get 
+            {
+                if (instance == null)
+                {
+                    instance = new QLCFEntities();
+                }    
+
+                return instance; 
+            }
+            set 
+            { 
+                instance = value; 
+            }
+        }
+
+    }
+}
