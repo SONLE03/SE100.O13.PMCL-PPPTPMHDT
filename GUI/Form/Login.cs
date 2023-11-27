df9a40a1f0f929cd124fe75ba0c807cadb0bc3b2
SONLE03 <sonle102003@gmail.com>
2023-11-27 10:36:34 +0700

    update app.config

--- GUI/Form/Login.cs
+++ GUI/Form/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BUS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace GUI
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string userName = txtUsername.Text.ToString();
+            string passWord = txtPass.Text.ToString();
+
+            if (BUSUser.Instance.GetUserByUsername(userName) != null)
+            {
+                MessageBox.Show("Hello " + userName + ".");
+            }
+            else
+            {
+                MessageBox.Show("Failed to authenticate, please check your username and password");
+            }
         }
     }
 }
