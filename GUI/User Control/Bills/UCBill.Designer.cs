e0857f2dbe758d8d6cfcfa085422e8de8931f47c
Thông Phạm Duy <21522645@gm.uit.edu.vn>
2023-12-18 14:13:46 +0700

    setting

--- GUI/User Control/Bills/UCBill.Designer.cs
+++ GUI/User Control/Bills/UCBill.Designer.cs
@@ -426,9 +426,9 @@
             this.label4.Location = new System.Drawing.Point(16, 37);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 21);
+            this.label4.Size = new System.Drawing.Size(36, 21);
             this.label4.TabIndex = 47;
-            this.label4.Text = "Surcharge";
+            this.label4.Text = "Tax";
             // 
             // lbSubTotal
             // 
